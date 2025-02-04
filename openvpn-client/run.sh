b4446d3bc75d76c129ba3f2bdb0d7200db0946ab
Maxime POUVREAU <mpouvreau@osmozis.com>
2025-02-04 22:04:49 +0100

    openvpn-client

---
+++ openvpn-client/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "$CA_CRT" > /etc/openvpn/ca.crt
+echo "$CLIENT_CRT" > /etc/openvpn/client.crt
+echo "$CLIENT_KEY" > /etc/openvpn/client.key
+echo "$OPENVPN_CONFIG" > /etc/openvpn/client.conf
+exec openvpn --config /etc/openvpn/client.conf
