226a562cb496c881d7ea3c919fad5ce62ca55fc5
Maxime POUVREAU <mpouvreau@osmozis.com>
2025-02-04 22:24:13 +0100

    openvpn-client

--- openvpn-client/run.sh
+++ openvpn-client/run.sh
@@ -3,6 +3,7 @@
 echo "$CA_CRT" > /etc/openvpn/ca.crt
 echo "$CLIENT_CRT" > /etc/openvpn/client.crt
 echo "$CLIENT_KEY" > /etc/openvpn/client.key
-echo -e "$USERNAME\n$PASSWORD" > /etc/openvpn/auth.txt
+echo -e "$USERNAME\n$PASSWORD" > /etc/openvpn/credentials.conf
 echo "$OPENVPN_CONFIG" > /etc/openvpn/client.conf
+echo -e "\n\nauth-user-pass credentials.conf" > /etc/openvpn/client.conf
 exec openvpn --config /etc/openvpn/client.conf
